887351b49ad356e42fc8a3a95e980bc104d81fd1
Dennis <309233594@qq.com>
2024-04-15 17:38:20 +0800

    04:运动控制-行走加速和减速
    使用 move_toward 函数进行加速和减速

--- player.gd
+++ player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
-const RUN_SPEED := 200.0
-const JUMP_VELOCITY := - 300.0
+const RUN_SPEED := 160.0
+#需要 0.2 秒后到达这个速度
+const ACCELERATION := RUN_SPEED / 0.2
+const JUMP_VELOCITY := -320.0
 
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -10,7 +12,11 @@ var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 
 func _physics_process(delta:float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
-	velocity.x = direction * RUN_SPEED
+	
+	#move_toward 起始值到目标值，速度的变化量
+	#速度的变化量 = 加速度 * 时间
+	#使用该方法后，人物会缓慢加速和减速，效果就是停止后，会往前滑行一段
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, ACCELERATION * delta)
 	
 	#设置重力，
 	velocity.y += gravity * delta
