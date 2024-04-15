65a68f96c6a2707fcd5f023900d6dd950e311961
Dennis <309233594@qq.com>
2024-04-15 22:47:46 +0800

    05:状态机--动画累加
    1、定义 StateMachine.gd  状态机脚本
    2、通过添加新建的这个状态机脚本到节点
    3、然后调用父节点方法，用来父节点通知状态机改变状态，状态机修改状态后在通知父节点进行后续操作，通过这种近似迭代的效果实现了动画嵌套

---
+++ classes/StateMachine.gd
@@ -0,0 +1,21 @@
+extends Node
+
+class_name StateMachine
+
+var current_state: int = -1:
+	set(v):
+		owner.transition_state(current_state, v)
+		current_state = v
+
+func _ready() -> void:
+	await owner.ready
+	current_state = 0
+	
+func _physics_process(delta: float) -> void:
+	while true:
+		var next := owner.get_next_state(current_state) as int
+		if current_state == next:
+			break
+		current_state = next
+	
+	owner.tick_physics(current_state, delta)
--- player.gd
+++ player.gd
@@ -1,12 +1,24 @@
 extends CharacterBody2D
 
+enum State {
+	IDLE,
+	RUNNING,
+	JUMP,
+	FALL,
+	LANDING,
+}
+
+const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
+
 const RUN_SPEED := 160.0
 #需要 0.2 秒后到达这个速度
 const FLOOR_ACCELERATION := RUN_SPEED / 0.5
 const AIR_ACCELERATION := RUN_SPEED / 0.02
 const JUMP_VELOCITY := -320.0
 
-var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var is_first_tick := false
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var coyote_timer: Timer = $CoyoteTimer
@@ -23,46 +35,107 @@ func _unhandled_input(event: InputEvent) -> void:
 		if velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
+func tick_physics(state: State, delta:float) -> void:
+	match state:
+		State.IDLE:
+			move(default_gravity, delta)
+		State.RUNNING:
+			move(default_gravity, delta)
+		State.JUMP:
+			move(0.0 if is_first_tick else default_gravity, delta)
+		State.FALL:
+			move(default_gravity, delta)
+		State.LANDING:
+			stand(delta)
+	
+	is_first_tick = false
+		
 
-func _physics_process(delta:float) -> void:
+func move(gravity: float, delta:float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	#move_toward 起始值到目标值，速度的变化量
 	#速度的变化量 = 加速度 * 时间
 	#使用该方法后，人物会缓慢加速和减速，效果就是停止后
 	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
-	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, FLOOR_ACCELERATION * delta)
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta)
 	
 	#设置重力
 	velocity.y += gravity * delta
 	
-	#如果离开了地面，并且定期器大于 0，说明是掉入悬崖掉下去了
-	var can_jum := is_on_floor() or coyote_timer.time_left > 0
-	var should_jump := can_jum and jump_request_timer.time_left > 0
-	if should_jump:
-		#如果快掉下去的瞬间，按住跳跃键的话，不让角色掉下去，而是跳跃
-		velocity.y = JUMP_VELOCITY
-		#离开地面瞬间起跳后，需要停止动画播放
-		coyote_timer.stop()
-		jump_request_timer.stop()
-	
-	if is_on_floor():
-		if is_zero_approx(direction) and is_zero_approx(velocity.x):
-			animation_player.play("idle")
-		else:
-			animation_player.play("runing")
-			
 	if not is_zero_approx(direction):
 		sprite_2d.flip_h = direction < 0
-		
-	var was_on_floor := is_on_floor()
 	
 	move_and_slide()
+
+
+func stand(delta: float)-> void:
+	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	velocity.x = move_toward(velocity.x, 0.0, acceleration * delta)
+	velocity.y += default_gravity * delta
 	
-	if is_on_floor() != was_on_floor:
-		#如果角色离开了地板，并且不是跳跃的话，启动定时器
-		#否则说明是角色主动跳跃所离开地面，不应该启动定时器
-		if was_on_floor and not should_jump:
-			coyote_timer.start()
-		else:
+	move_and_slide()
+
+
+			
+func get_next_state(state: State) -> State:
+	var can_jump := is_on_floor() or coyote_timer.time_left > 0
+	var should_jump := can_jump and jump_request_timer.time_left > 0
+	if should_jump:
+		return State.JUMP
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	
+	match state:
+		State.IDLE:
+			if not is_on_floor():
+				return State.FALL
+			if not is_still:
+				return State.RUNNING
+		
+		State.RUNNING:
+			if not is_on_floor():
+				return State.FALL
+			if is_still:
+				return State.IDLE
+		
+		State.JUMP:
+			if velocity.y >= 0:
+				return State.FALL
+		
+		State.FALL:
+			if is_on_floor():
+				return State.LANDING if is_still else State.RUNNING
+		
+		State.LANDING:
+			if not animation_player.is_playing():
+				return State.IDLE
+	
+	return state
+
+
+func transition_state(from: State, to: State)-> void:
+	if from not in GROUND_STATES and to in GROUND_STATES:
+		coyote_timer.stop()
+		
+	match to:
+		State.IDLE:
+			animation_player.play("idle")
+		State.RUNNING:
+			animation_player.play("runing")
+		State.JUMP:
+			animation_player.play("jump")
+			#如果快掉下去的瞬间，按住跳跃键的话，不让角色掉下去，而是跳跃
+			velocity.y = JUMP_VELOCITY
+			#离开地面瞬间起跳后，需要停止动画播放
 			coyote_timer.stop()
+			jump_request_timer.stop()
+		State.FALL:
+			animation_player.play("fall")
+			if from in GROUND_STATES:
+				coyote_timer.start()
+		State.LANDING:
+			animation_player.play("landing")
+				
+	is_first_tick = true
