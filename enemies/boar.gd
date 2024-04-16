9d16732fa30b97981d70a9c9f6745fcfc9f0abfb
Dennis <309233594@qq.com>
2024-04-16 21:45:54 +0800

    09:三段攻击

--- enemies/boar.gd
+++ enemies/boar.gd
@@ -51,11 +51,11 @@ func get_next_state(state: State) -> State:
 	return state
 	
 func transition_state(from:State, to:State) -> void:
-	print("[%s] %s => %s" %[
-		Engine.get_physics_frames(),
-		State.keys()[from] if from != -1 else "<START>",
-		State.keys()[to],
-	])
+	#print("[%s] %s => %s" %[
+		#Engine.get_physics_frames(),
+		#State.keys()[from] if from != -1 else "<START>",
+		#State.keys()[to],
+	#])
 	
 	match to:
 		State.IDLE:
