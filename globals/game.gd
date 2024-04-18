78f836aedd7cef2484b3e22a00d48e4158c86a62
Dennis <309233594@qq.com>
2024-04-18 18:24:00 +0800

    16:转场效果-转场时暂停所有功能

--- globals/game.gd
+++ globals/game.gd
@@ -8,9 +8,11 @@ func _ready() -> void:
 
 func change_scene(path: String, entry_point: String) -> void:
 	var tree := get_tree()
+	tree.paused = true
 	
 	var tween := create_tween()
-	tween.tween_property(color_rect, "color:a", 1, 1.2)
+	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween.tween_property(color_rect, "color:a", 1, 0.8)
 	await tween.finished
 	
 	tree.change_scene_to_file(path)
@@ -21,6 +23,7 @@ func change_scene(path: String, entry_point: String) -> void:
 		if node.name == entry_point:
 			tree.current_scene.update_player(node.global_position, node.direction)
 			break
-			
+	
+	tree.paused = false
 	tween = create_tween()
 	tween.tween_property(color_rect, "color:a", 0, 0.2)
